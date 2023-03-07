a9c52bb121d35f39ae0035b95ccfe895e37483da
ElMaestruli28 <123603377+ElMaestruli28@users.noreply.github.com>
2023-03-07 15:34:20 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+
+document.querySelector(".menu-btn").addEventListener('click', () => {
+    document.querySelector('.nav-menu').classList.toggle('show')
+});
+  
+  ScrollReveal().reveal('.showcase');
+  ScrollReveal().reveal('.news-cards-two', { delay: 300 });
+  ScrollReveal().reveal('.content-two', { delay: 300 });
+  ScrollReveal().reveal('.social', { delay: 300 });
+  ScrollReveal().reveal('.footer-links', { delay: 600 });
+  ScrollReveal().reveal('.footer', { delay: 500 });
